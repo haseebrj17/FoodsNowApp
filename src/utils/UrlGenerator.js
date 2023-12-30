084d63f44f6c237c658760ae57af77b76062f0c2
haseebrj17 <muhamadhaseeb2001@gmail.com>
2023-12-30 13:11:42 +0500

    Refactor image URL transformation logic

--- src/utils/UrlGenerator.js
+++ src/utils/UrlGenerator.js
@@ -1,6 +1,18 @@
 export const transformImageUrl = ({ originalUrl, size }) => {
     const baseUrl = `https://ik.imagekit.io/FoodsNowGmbH${size}/sliders/`;
+
+    // Split the original URL to get the parts after the domain
     const urlParts = originalUrl.split('/');
-    const relevantParts = urlParts.slice(4);
-    return baseUrl + relevantParts.join('/');
-}
+
+    // Decode each part to transform non-ASCII characters to their actual characters
+    const decodedParts = urlParts.map(part => decodeURIComponent(part));
+
+    // Extract the relevant parts (assuming they start from the 4th segment in the array)
+    const relevantParts = decodedParts.slice(4);
+
+    // Join the parts back into a URL string
+    const transformedUrl = baseUrl + relevantParts.join('/');
+
+    console.log(transformedUrl);
+    return transformedUrl;
+};
